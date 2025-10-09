6a8fb1c83a4430cd3fbc125365faa6e91868ab0e
michelepatella <patellamichele2002@gmail.com>
2025-10-09 23:52:13 +0200

    feat: Refactor CacheConfig (main)
    Simplifies `CacheConfig` by removing LSTM-specific
    configurations and moving `dimension` and `ttl` to
    the top level. Updates `config.yaml` to reflect these
    changes.

--- src/config/classes/SimulationConfig.py
+++ src/config/classes/SimulationConfig.py
@@ -1,28 +1,6 @@
 from pydantic import BaseModel, confloat, conint
 
 
-# Simulation — Cache — General
-class CacheGeneralConfig(BaseModel):
-    dimension: conint(gt=0)
-    ttl: conint(gt=0)
-
-
-# Simulation — Cache — LSTM — Confidence Intervals
-class ConfidenceIntervalsConfig(BaseModel):
-    level: confloat(ge=0, le=1)
-
-
-class MCDropoutConfig(BaseModel):
-    samples: conint(gt=0)
-
-
-# Simulation — Cache — LSTM — Main Config
-class LSTMCacheConfig(BaseModel):
-    prediction_interval: conint(gt=0)
-    confidence_intervals: ConfidenceIntervalsConfig
-    mc_dropout: MCDropoutConfig
-
-
 # Simulation — Metrics — Mistake Rate
 class MistakeRateConfig(BaseModel):
     window: conint(gt=0)
@@ -32,10 +10,10 @@ class MetricsConfig(BaseModel):
     mistake_rate: MistakeRateConfig
 
 
-# Simulation — Main Config
+# Simulation —  Cache
 class CacheConfig(BaseModel):
-    general: CacheGeneralConfig
-    lstm: LSTMCacheConfig
+    dimension: conint(gt=0)
+    ttl: conint(gt=0)
 
 
 class SimulationConfig(BaseModel):
