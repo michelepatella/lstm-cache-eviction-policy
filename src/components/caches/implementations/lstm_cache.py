Feat: Optimize cache performance and fix Belady MIN calculation
This commit includes several changes to improve the performance
of the cache and fix the calculation of the Belady MIN metric.

- Reduces cache dimension in config.yaml
- Fixes Belady MIN calculation in simulator.py
- Adds error handling to LFU cache
- Accesses the key if found in cache in base_cache.py
- Changes testing_set type to pd.DataFrame and fixes extraction
  of last rows from dataset in lstm_cache.py and
  lasts_extractor.py

src/components/caches/implementations/lstm_cache.py
@@ -2,9 +2,9 @@ import random
 from http.client import HTTPException
 from typing import Any
 
+import pandas as pd
 import requests
 from box import Box
-from torch.utils.data import DataLoader
 
 from components.caches.implementations.utils.base_cache import BaseCache
 from components.caches.utils.cache_metrics_logger import (
@@ -161,7 +161,7 @@ class LSTMCache(BaseCache):
         key: int,
         current_time: float,
         current_idx: int,
-        testing_set: DataLoader,
+        testing_set: pd.DataFrame,
         config: Any,
     ) -> None:
         """Insert a key in the LSTM cache.
@@ -176,7 +176,7 @@ class LSTMCache(BaseCache):
             key (int): Key to insert.
             current_time (float): Current time.
             current_idx (int): Index of the current request.
-            testing_set (DataLoader): Testing dataset for sequence extraction.
+            testing_set (pd.DataFrame): Testing dataset for sequence extraction.
             config (Config): Configuration object.
 
         Returns:
@@ -205,7 +205,7 @@ class LSTMCache(BaseCache):
                 last_accesses = extract_last_rows_from_dataset(
                     current_idx,
                     seq_len,
-                    testing_set,
+                    testing_set.data,
                 )
 
                 # Check whether last accesses

src/components/dataset/rows/extractions/lasts_extractor.py
@@ -1,4 +1,4 @@
-from torch.utils.data import DataLoader
+import pandas as pd
 
 from components.const import (
     TIME_SECONDS_IN_HOUR,
@@ -19,7 +19,7 @@ from components.time.transforms.trig_decoder import (
 def extract_last_rows_from_dataset(
     current_idx: int,
     seq_len: int,
-    df: DataLoader,
+    df: pd.DataFrame,
     time_conversion_factor: float = TIME_SECONDS_IN_HOUR,
 ) -> list[tuple[float, int]] | None:
     """Extract the last rows from a dataset.
@@ -30,7 +30,7 @@ def extract_last_rows_from_dataset(
     Args:
         current_idx (int): Current dataset index.
         seq_len (int): Sequence length to extract.
-        df (DataLoader): Dataset to extract rows from.
+        df (pd.DataFrame): Dataset to extract rows from.
         time_conversion_factor (float): Factor to convert extracted seconds to
                                         desired unit.
 
@@ -51,7 +51,7 @@ def extract_last_rows_from_dataset(
         # Extract a sliding window from dataset
         # of sequence length size
         window_df = extract_sliding_window_dataset_rows(
-            df.data,
+            df,
             current_idx,
             seq_len,
         )