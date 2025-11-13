d2177841d5cadd2414f6192ddf7c6a902c873df0
michelepatella <patellamichele2002@gmail.com>
2025-11-13 18:31:07 +0100

    fix: Fixed dataset training-validation splits (bugfix)
    This commit refactors the dataset splitting logic to return
    AccessLogsDataset objects instead of PyTorch Subsets. This
    change ensures that the resulting training and validation sets
    are proper AccessLogsDataset instances, maintaining data
    integrity and functionality. Additionally, it addresses an
    issue related to index handling during dataset splitting.
    The index is reset to avoid errors.

--- src/pipeline/steps/data_preprocessor.py
+++ src/pipeline/steps/data_preprocessor.py
@@ -47,7 +47,7 @@ from pipeline.const import (
     DAGS_HUB_DVC,
     DAGS_HUB_REPO_NAME,
     DAGS_HUB_REPO_OWNER,
-    DATASET_CHUNK_RESET_INDEX_DROP,
+    DATASET_RESET_INDEX_DROP,
     DATASET_PROCESSED_TYPE,
     LOGS_PHASE_DATA_PREPROCESSING,
 )
@@ -149,7 +149,7 @@ def preprocess_data() -> None:
         # working on a dataset chunk
         futures = [
             build_features_task.remote(
-                chunk.reset_index(drop=DATASET_CHUNK_RESET_INDEX_DROP),
+                chunk.reset_index(drop=DATASET_RESET_INDEX_DROP),
                 seq_len,
             )
             for chunk in df_chunks
